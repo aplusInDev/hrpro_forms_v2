f98cee977325d3ddaf65b08de3b5e0048120da90
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-04-18 18:50:24 +0100

    step 1 done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { FName, FType, FIsRequired, FDefaultValue } from './components/ui'
 import { CustomOptions } from './components'
 
 export default function App() {
   return (
     <>
+      <FName />
+      <FType types={types}/>
+      <FIsRequired />
+      <FDefaultValue />
       <CustomOptions />
     </>
   )
 }
+
+const types = [
+  "text", "number", "email", "password",
+  "date", "time", "textarea", "select",
+  "checkbox", "radio", "file", "tel"
+]
--- src/components/ui/FType.jsx
+++ src/components/ui/FType.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 
 export default function FType({ types }) {
+  let options = null;
+
+  if (types) {
+    options = types.map((type, index) => {
+      return <option key={index} value={type}>{type}</option>
+    });
+  }
+
   return (
     <select>
-      {types.map(type => (
-        <option key={type} value={type}>{type}</option>
-      ))}
+      {options}
     </select>
   );
 }
