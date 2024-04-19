a4989923c39966d909132c3e9e19ceeaf4cf85fe
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-04-19 22:19:14 +0100

    form request done

--- src/components/ui/FormInfo.jsx
+++ src/components/ui/FormInfo.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import { React, useEffect, useState } from 'react'
 import '../../assets/css/FormInfo.css'
 
+const company_id = localStorage.getItem('company_id');
+const allFormsUrl = `http://localhost:5000/api/v1/companies/${company_id}/forms`;
+
 export default function FormInfo() {
+  const [forms, setForms] = useState([]);
+  const [form, setForm] = useState({});
+
+  useEffect(() => {
+    const fetchForms = async () => {
+      const forms = await getForms();
+      if (forms) {
+        setForms(forms);
+        setForm(forms[0]);
+      }
+    };
+    fetchForms();
+  }, []);
+
+  async function handleAddForm(e) {
+    e.preventDefault();
+    const formInfo = {
+      name: document.querySelector('input[name="form-title"]').value,
+      description: document.querySelector('textarea[name="form-description"]').value,
+    };
+    const form = await postForm(formInfo);
+    if (form) {
+      setForms([...forms, form]);
+      setForm(form);
+    }
+  }
+
+  async function handleUpdateForm(e) {
+    e.preventDefault();
+    const newForm = await putForm({name: form.name, description: form.description, uri: form.uri});
+    if (newForm) {
+      setForm(newForm);
+      setForms(forms.map(f => f.id === newForm.id ? newForm : f));
+    }
+  }
+
+
   return (
     <form className='form-info'>
       <span>
@@ -9,23 +49,99 @@ export default function FormInfo() {
         <select
           name='form-name'
           aria-label='form-name'
+          value={form.id}
+          onChange={(e) => {
+            const formId = e.target.value;
+            const form = forms.find(form => form.id === formId);
+            setForm(form);
+          }}
         >
-          <option value="1">form 1</option>
-          <option value="2">form 2</option>
-          <option value="3">form 3</option>
+          {forms.map((form, index) => {
+            return (
+              <option key={index} value={form.id}>{form.name}</option>
+            );
+          })}
         </select>
         <h3>form description: </h3>
         <textarea name="form-description"
-          defaultValue='form description!'
+          placeholder='form description!'
           aria-label='form-description'
           id="2" cols="20" rows="2"
+          value={form.description || ''}
+          onChange={(e) => {
+            setForm({ ...form, description: e.target.value });
+          }}
         >
         </textarea>
       </span>
       <span>
-        <input type='text' name='form-title' placeholder='form name' />
-        <input type='submit' value='Add Form' />
+        <input type='text'
+          name='form-title'
+          placeholder='form name'
+        />
+        <input type='submit'
+          value='Add Form'
+          onClick={handleAddForm}
+        />
+        <input type='submit'
+          value='Update Form'
+          onClick={handleUpdateForm}
+        />
       </span>
     </form>
   );
+}
+
+async function getForms() {
+  try {
+    const response = await fetch(allFormsUrl);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  } finally {
+    console.log('completed');
+  }
+}
+
+async function postForm(info) {
+  try {
+    const response = await fetch(allFormsUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(info)
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  } finally {
+    console.log('completed');
+  }
+}
+
+async function putForm(info) {
+  try {
+    const response = await fetch(info.uri, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(info)
+    });
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  } finally {
+    console.log('completed');
+  }
 }
--- src/pages/CustomForm.jsx
+++ src/pages/CustomForm.jsx
@@ -2,6 +2,8 @@ import { React } from 'react'
 import { FormInfo } from '../components/ui'
 import { FieldsTable, NewField } from '../components';
 
+localStorage.setItem('company_id', 'b344d0e5-6a02-472a-9c82-dc7c9e57d3f1');
+
 export default function CustomForm() {
 
   return (
