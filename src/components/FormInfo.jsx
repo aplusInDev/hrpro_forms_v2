7d6dd6c2cd7229f3918c8c275fa59c916be31f5f
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-04-20 07:41:55 +0100

    post form done

--- src/components/FormInfo.jsx
+++ src/components/FormInfo.jsx
@@ -5,10 +5,15 @@ import '../assets/css/CustomForm.css'
 
 const company_id = localStorage.getItem('company_id');
 const allFormsUrl = `http://localhost:5000/api/v1/companies/${company_id}/forms`;
+const initialForm = {
+  name: '',
+  description: ''
+}
 
 export default function FormInfo() {
   const [forms, setForms] = useState([]);
-  const [form, setForm] = useState({});
+  const [form, setForm] = useState(initialForm);
+  const [newForm, setNewForm] = useState(initialForm);
 
   useEffect(() => {
     const fetchForms = async () => {
@@ -21,8 +26,15 @@ export default function FormInfo() {
     fetchForms();
   }, []);
 
-  // function handleAdd(name, description) {
-  // }
+  async function handleAdd(e) {
+    e.preventDefault();
+    const added_form = await postForm(allFormsUrl, newForm);
+    if (added_form) {
+      setForms([...forms, added_form]);
+      setForm(added_form);
+      setNewForm(initialForm);
+    }
+  }
 
   async function handleEdit(form) {
     const updated_form = await putForm({
@@ -82,8 +94,36 @@ export default function FormInfo() {
           Update Form
         </button>
       </form>
-      <form className='new-form'>
-      </form>
+      <NewForm newForm={newForm} setNewForm={setNewForm} onAdd={handleAdd} />
     </div>
   );
+}
+
+function NewForm({ newForm, setNewForm , onAdd }) {
+  return (
+    <>
+      <form className='new-form'>
+        <input
+          name='new-form-name'
+          type='text'
+          placeholder='New Form Name'
+          aria-label='new-form-name'
+          value={newForm.name}
+          onChange={(e) => setNewForm({ ...newForm, name: e.target.value })}
+        />
+        <textarea
+          name='new-form-description'
+          placeholder='New Form Description'
+          aria-label='new-form-description'
+          value={newForm.description}
+          onChange={(e) => setNewForm({ ...newForm, description: e.target.value })}
+        />
+        <button type='submit'
+          onClick={onAdd}
+        >
+          Create Form
+        </button>
+      </form>
+    </>
+  )
 }
