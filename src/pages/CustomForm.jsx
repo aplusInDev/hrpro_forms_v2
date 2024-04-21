ab85e50a319678a6538401ebdcc2a1afa0be2fd4
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-04-21 14:16:06 +0100

    removing old form info

--- src/pages/CustomForm.jsx
+++ src/pages/CustomForm.jsx
@@ -1,5 +1,4 @@
 import { React, useState } from 'react'
-// import { FormInfo } from '../components/ui'
 import { FormInfo, FieldsTable, NewField } from '../components';
 
 import '../assets/css/CustomForm.css'
