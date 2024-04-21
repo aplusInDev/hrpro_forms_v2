2d8b7dca9e6d3e043f9cfc745c32ca32268b4649
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-04-21 18:02:42 +0100

    removing unimportant files

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,7 +1,5 @@
-export { default as FName } from './FName';
 export { default as FType } from './FType';
 export { default as FIsRequired } from './FIsRequired';
-export { default as FDefaultValue } from './FDefaultValue';
 export { default as FDescription } from './FDescription';
 export { default as Example } from './Example';
 export { default as FOption } from './FOption';
