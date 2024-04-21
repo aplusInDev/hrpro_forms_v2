6c6be5d070990bb75909510dde8a63ef0d71014a
aplusInDev <laabid.abdessamadplus@gmail.com>
2024-04-21 14:15:39 +0100

    removing old FormInfo

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -4,6 +4,5 @@ export { default as FIsRequired } from './FIsRequired';
 export { default as FDefaultValue } from './FDefaultValue';
 export { default as FDescription } from './FDescription';
 export { default as Example } from './Example';
-export { default as FormInfo } from './FormInfo';
 export { default as FOption } from './FOption';
 export { default as FText } from './FText';
--- src/services/fieldService.js
+++ src/services/fieldService.js
@@ -18,3 +18,15 @@ export async function postField(id, info) {
     console.log('completed');
   }
 }
+
+export async function getFields(formId) {
+  const url = `http://localhost:5000/api/v1/forms/${formId}/fields`;
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
